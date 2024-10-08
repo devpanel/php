99412466d0076a0b1af8791efa972d3013d3ebcc
levi-devpanel <levi@devpanel.com>
2024-10-08 19:15:46 +0700

    DP-1094 make code server with the token

--- 8.3/base/scripts/apache-start.sh
+++ 8.3/base/scripts/apache-start.sh
@@ -41,7 +41,11 @@ if [[ "$CODES_ENABLE" == "yes" ]]; then
 # Start the primary process and put it in the background
 sudo -E apache2-foreground &
 # Start the helper process
+if [[ "$CODES_AUTH" == "yes" ]]; then
+sudo -u www -E -- code-server --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR --user-data-dir=$CODES_USER_DATA_DIR
+else
 sudo -u www -E -- code-server --auth none --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR --user-data-dir=$CODES_USER_DATA_DIR
+fi
 # and leave it there
 fg %1
 else
