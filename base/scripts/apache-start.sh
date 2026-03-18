cd99ef24464ba18dca916f7b8880bdfbb7b9ea57
Darren Oh <2293701+darrenoh@users.noreply.github.com>
2026-03-18 09:21:12 -0400

    Refactor apache-start.sh for clarity and updates
    Fixed indentation
    Fixed custom package installation never happening because it came after Apache start

--- base/scripts/apache-start.sh
+++ base/scripts/apache-start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Turn on bash's job control
+# Turn on bash's job control.
 
 cp /templates/apache2.conf /etc/apache2/apache2.conf
 cp /templates/000-default.conf /etc/apache2/sites-enabled/000-default.conf
 cp /templates/php.ini ${PHP_EXT_DIR}/zz-www.ini
 
-# Substitute in php.ini values
+# Substitute in php.ini values.
 [ ! -z "${PHP_CLEAR_ENV}" ] && sed -i "s|{{PHP_CLEAR_ENV}}|${PHP_CLEAR_ENV}|" ${PHP_EXT_DIR}/zz-www.ini
 [ ! -z "${PHP_MEMORY_LIMIT}" ] && sed -i "s|{{PHP_MEMORY_LIMIT}}|${PHP_MEMORY_LIMIT}|" ${PHP_EXT_DIR}/zz-www.ini
 [ ! -z "${PHP_UPLOAD_MAX_FILESIZE}" ] && sed -i "s|{{PHP_UPLOAD_MAX_FILESIZE}}|${PHP_UPLOAD_MAX_FILESIZE}|" ${PHP_EXT_DIR}/zz-www.ini
@@ -14,53 +14,46 @@ cp /templates/php.ini ${PHP_EXT_DIR}/zz-www.ini
 [ ! -z "${PHP_MAX_INPUT_TIME}" ] && sed -i "s|{{PHP_MAX_INPUT_TIME}}|${PHP_MAX_INPUT_TIME}|" ${PHP_EXT_DIR}/zz-www.ini
 [ ! -z "${PHP_MAX_INPUT_VARS}" ] && sed -i "s|{{PHP_MAX_INPUT_VARS}}|${PHP_MAX_INPUT_VARS}|" ${PHP_EXT_DIR}/zz-www.ini
 
-# Add custom php.ini if it exists
+# Add custom php.ini if it exists.
 [ -f "$PHP_CUSTOM_INI" ] && cp $PHP_CUSTOM_INI ${PHP_EXT_DIR}/zzz-www-custom.ini
 
-# Custom Environment variables in /etc/apache2/sites-enabled/000-default.conf
+# Custom Environment variables in /etc/apache2/sites-enabled/000-default.conf.
 [ ! -z "$APP_ROOT" ] && sed -i "s|{{APP_ROOT}}|${APP_ROOT}|" /etc/apache2/sites-enabled/000-default.conf
 [ ! -z "$WEB_ROOT" ] && sed -i "s|{{WEB_ROOT}}|${WEB_ROOT}|" /etc/apache2/sites-enabled/000-default.conf
 [ ! -z "$SERVER_NAME" ] && sed -i "s|{{SERVER_NAME}}|${SERVER_NAME}|" /etc/apache2/sites-enabled/000-default.conf
-# Replace // by /
+# Replace // by /.
 sed -i "s/\/\//\//g" /etc/apache2/sites-enabled/000-default.conf
 
-# install Drush 7, 8, 9, 10, 11
+# install Drush 7, 8, 9, 10, 11.
 /bin/bash source ~/.bashrc
 
-# Configure code server
-if [[ ! -d "$CODES_USER_DATA_DIR" ]]; then
-  mkdir -p "$CODES_USER_DATA_DIR"
-  chown -R www:www "$CODES_USER_DATA_DIR"
-fi
+# Install any custom packages.
+[ -f "$APP_ROOT/.devpanel/custom_package_installer.sh" ] && /bin/bash $APP_ROOT/.devpanel/custom_package_installer.sh  >> /tmp/custom_package_installer.log
 
 set -m
 if [[ "$CODES_ENABLE" == "yes" ]]; then
-# Start the primary process and put it in the background
-apache2-foreground &
-# Start the helper process
-if [[ "$CODES_AUTH" == "yes" ]]; then
-sudo -u www -E -- code-server --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR --user-data-dir=$CODES_USER_DATA_DIR
-else
-sudo -u www -E -- code-server --auth none --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR --user-data-dir=$CODES_USER_DATA_DIR
-fi
-# and leave it there
-fg %1
-else
-# Start the primary process and put it in the background
-apache2-foreground
-fi
+  # Install the GitHub Copilot Chat extension and any user-specified VSCode extensions.
+  sudo -u www -E -- code-server --install-extension /usr/local/share/devpanel/copilot-chat.vsix
+  if [ -n "${DP_VSCODE_EXTENSIONS:-}" ]; then
+    (
+      IFS=','
+      for value in $DP_VSCODE_EXTENSIONS; do
+        sudo -u www -E -- code-server --install-extension "$value"
+      done
+    )
+  fi
 
-# Install GitHub Copilot Chat extension and any user-specified VSCode extensions
-code-server --install-extension /usr/local/share/devpanel/copilot-chat.vsix --user-data-dir="$CODES_USER_DATA_DIR"
-if [ -n "${DP_VSCODE_EXTENSIONS:-}" ]; then
-  chown -R www:www "$CODES_USER_DATA_DIR/extensions"
-  (
-    IFS=','
-    for value in $DP_VSCODE_EXTENSIONS; do
-      code-server --install-extension "$value" --user-data-dir="$CODES_USER_DATA_DIR"
-    done
-  )
+  # Start the primary process and put it in the background.
+  apache2-foreground &
+  # Start the helper process.
+  if [[ "$CODES_AUTH" == "yes" ]]; then
+    sudo -u www -E -- code-server --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR
+  else
+    sudo -u www -E -- code-server --auth none --port $CODES_PORT --host 0.0.0.0 $CODES_WORKING_DIR
+  fi
+  # Now bring it to the foreground.
+  fg %1
+else
+  # Start the primary process.
+  apache2-foreground
 fi
-
-# Install custom packages if have
-[ -f "$APP_ROOT/.devpanel/custom_package_installer.sh" ] && /bin/bash $APP_ROOT/.devpanel/custom_package_installer.sh  >> /tmp/custom_package_installer.log
